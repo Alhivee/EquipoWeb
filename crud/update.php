33c095057744056ab4faa2b5e09a0ccc08d0482a
Alhivee <103689996+Alhivee@users.noreply.github.com>
2022-12-02 01:48:59 -0600

    v

--- crud/update.php
+++ crud/update.php
@@ -8,11 +8,33 @@ $estatus=$_POST['estatus'];
 $tipo = $_GET['tipo'];
 $idu = $_GET['idu'];
 $idGUIA = $_GET['id'];
-
+$correo = $_POST['correo_remitente'];
 $sql="UPDATE `softteci_paqueteria`.`PAQUETE` SET `estatus` = '$estatus' WHERE `ID_GUIA` = $guia ";
 $query=mysqli_query($con,$sql);
 
-
+if($estatus=='En confirmacion'){
+       $asunto = "Confirmación - Envio de paquete";
+    
+ /* $cuerpo = "  
+    HOLA $nombre_remitente! Con este correo confirmamos que realizaste un envio de paqueteria,  ¡Gracias por su compra! Su numero de Guia es: $id "  ;  */
+   // $remitente = "FROM:". "envios@paqueteria.softtecisc.com";
+    $remitente = 'MIME-Version: 1.0' . "\r\n";
+                          $remitente .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+   $cuerpo = ' <html lang="es">
+                             <head> <meta charset="UTF-8" /> </head>
+                              <body>
+                              HOLA! Con este correo queremos corroborar que tu paquete ha sido entregado. 
+					 
+                              
+                                  <center><a href="https://www.paqueteria.softtecisc.com/crud/verificacion.php?id='.$id.'&E=1" target="Contenido" class="link"> <img src="https://cdn-icons-png.flaticon.com/512/6851/6851140.png" height=90px ></a> </center> <br>
+                          <br> En dado caso de no reconocer esta acción ponerse en contacto con el soporte tecnico. <br>
+                           ¡Gracias por su preferencia!
+                          </html></body>
+                              ';
+    
+    
+mail ($correo,$asunto,$cuerpo,$remitente);  
+}
 
     if($query){
         
--- crud/verificacion.php
+++ crud/verificacion.php
@@ -15,8 +15,13 @@ $conexion=mysqli_connect($servername, $username, $password, $dbname);
     
 $ID_GUIA= $_GET["id"];    
 $Validacion = "Validado";
+$entregado = $_GET['E'];
 $query = "UPDATE `softteci_paqueteria`.`PAQUETE` SET validacion = '$Validacion' WHERE ID_GUIA='$rfc'";
+if($entregado=1) {
+   $query = "UPDATE `softteci_paqueteria`.`PAQUETE` SET validacion = 'Entregado' WHERE ID_GUIA='$rfc'"; 
+}
 $Resultado= mysqli_query($conexion,$query);
+
 
 if($Resultado)
 { 
